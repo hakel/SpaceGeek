392bb6f60d5f359b5ecef2616fadcbe7e885ab31
hakel <scotthakel@gmail.com>
2018-11-12 16:25:46 -0500

    Add project files.

---
+++ SpaceGeek/Function.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization;
+using Alexa.NET.Response;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Newtonsoft.Json;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+/*
+ * http://matthiasshapiro.com/2017/02/10/tutorial-alexa-skills-in-c-the-code/
+ netdev
+Netdev1$ 
+ * 
+ */
+namespace SpaceGeek
+{
+    public class Function
+    {
+
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            SkillResponse response = new SkillResponse();
+            response.Response = new ResponseBody();
+            response.Response.ShouldEndSession = false;
+            IOutputSpeech innerResponse = null;
+            var log = context.Logger;
+            log.LogLine($"Skill Request Object:");
+            log.LogLine(JsonConvert.SerializeObject(input));
+
+            var allResources = Utilities.GetResources();
+            var resource = allResources.FirstOrDefault();
+
+            if (input.GetRequestType() == typeof(LaunchRequest))
+            {
+                log.LogLine($"Default LaunchRequest made: 'Alexa, open Science Facts");
+                innerResponse = new PlainTextOutputSpeech();
+                (innerResponse as PlainTextOutputSpeech).Text = Utilities.EmitNewFact(resource, true);
+
+            }
+            else if (input.GetRequestType() == typeof(IntentRequest))
+            {
+                var intentRequest = (IntentRequest)input.Request;
+
+                switch (intentRequest.Intent.Name)
+                {
+                    case "AMAZON.CancelIntent":
+                        log.LogLine($"AMAZON.CancelIntent: send StopMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
+                        response.Response.ShouldEndSession = true;
+                        break;
+                    case "AMAZON.StopIntent":
+                        log.LogLine($"AMAZON.StopIntent: send StopMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = resource.StopMessage;
+                        response.Response.ShouldEndSession = true;
+                        break;
+                    case "AMAZON.HelpIntent":
+                        log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = resource.HelpMessage;
+                        break;
+                    case "GetFactIntent":
+                        log.LogLine($"GetFactIntent sent: send new fact");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = Utilities.EmitNewFact(resource, false);
+                        break;
+                    case "GetNewFactIntent":
+                        log.LogLine($"GetFactIntent sent: send new fact");
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = Utilities.EmitNewFact(resource, false);
+                        break;
+                    default:
+                        log.LogLine($"Unknown intent: " + intentRequest.Intent.Name);
+                        innerResponse = new PlainTextOutputSpeech();
+                        (innerResponse as PlainTextOutputSpeech).Text = resource.HelpReprompt;
+                        break;
+                }
+            }
+
+            response.Response.OutputSpeech = innerResponse;
+            response.Version = "1.0";
+            log.LogLine($"Skill Response Object...");
+            log.LogLine(JsonConvert.SerializeObject(response));
+            return response;
+        }
+
+    }
+
+    
+}
---
+++ SpaceGeek/Utilities.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace SpaceGeek
+{
+    public class Utilities
+    {
+        public static List<FactResource> GetResources()
+        {
+            List<FactResource> resources = new List<FactResource>();
+
+            FactResource enUSResource = new FactResource("en-US");
+            enUSResource.SkillName = "American Science Facts";
+            enUSResource.GetFactMessage = "Here's your science fact: ";
+            enUSResource.HelpMessage = "You can say tell me a science fact, or, you can say exit... What can I help you with?";
+            enUSResource.HelpReprompt = "You can say tell me a science fact to start";
+            enUSResource.StopMessage = "Goodbye!";
+            enUSResource.Facts = GetFacts();
+
+            resources.Add(enUSResource);
+
+            return resources;
+        }
+
+        public static List<string> GetFacts()
+        {
+            List<string> facts = new List<string>();
+
+            string baseUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=cincinnati&sort=newest&api-key=5476602b245f4bc38ffbad32c1eb11f5";
+            //The 'using' will help to prevent memory leaks.
+            //Create a new instance of HttpClient
+            using (HttpClient client = new HttpClient())
+            {
+                var response = client.GetAsync(baseUrl).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = response.Content;
+
+                    // by calling .Result you are synchronously reading the result
+                    string responseString = responseContent.ReadAsStringAsync().Result;
+                    dynamic responseData = JsonConvert.DeserializeObject(responseString);
+
+                    int docCount = (int)responseData.response.docs.Count;
+
+                    for (int i = 0; i < docCount; i++)
+                    {
+                        string hl = responseData.response.docs[i].headline.main;
+                        facts.Add(hl);
+                    }
+                }
+            }
+
+            return facts;
+
+        }
+        public static List<string> GetFactsOld()
+        {
+            List<string> facts = new List<string>();
+
+            facts.Add("A year on Mercury is just 88 days long.");
+            facts.Add("Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.");
+            facts.Add("Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.");
+            facts.Add("On Mars, the Sun appears about half the size as it does on Earth.");
+            facts.Add("Earth is the only planet not named after a god.");
+            facts.Add("Jupiter has the shortest day of all the planets.");
+            facts.Add("The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.");
+            facts.Add("The Sun contains 99.86% of the mass in the Solar System.");
+            facts.Add("The Sun is an almost perfect sphere.");
+            facts.Add("A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.");
+            facts.Add("Saturn radiates two and a half times more energy into space than it receives from the sun.");
+            facts.Add("The temperature inside the Sun can reach 15 million degrees Celsius.");
+            facts.Add("The Moon is moving approximately 3.8 cm away from our planet every year.");
+
+            return facts;
+
+        }
+        public static string EmitNewFact(FactResource resource, bool withPreface)
+        {
+            Random r = new Random();
+            if (withPreface)
+            {
+                return resource.GetFactMessage + resource.Facts[r.Next(resource.Facts.Count)];
+            }
+            else
+            {
+                return resource.Facts[r.Next(resource.Facts.Count)];
+            }
+        }
+
+    }
+}
